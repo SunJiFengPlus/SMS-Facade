93142bdbc54262457a48c0d8b5b824829d42cab4
孙继峰 <sun.jifeng@outlook.com>
2020-04-24 08:14:38 +0800

    配置聚合数据短信模板

--- src/main/java/com/example/smsfacade/bean/JuheSenderConfigBean.java
+++ src/main/java/com/example/smsfacade/bean/JuheSenderConfigBean.java
@@ -2,11 +2,14 @@ package com.example.smsfacade.bean;
 
 import lombok.Data;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * @author 孙继峰
  * @since 2020/4/26
  */
 @Data
+@ConfigurationProperties(prefix = "juhe")
 public class JuheSenderConfigBean {
     private String appKey;
     private String url;
