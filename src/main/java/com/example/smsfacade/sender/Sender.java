b5b2e1d11fc22992434db0d89c7521f722e76f02
孙继峰 <sun.jifeng@outlook.com>
2020-05-14 09:28:05 +0800

    完善注释

--- src/main/java/com/example/smsfacade/sender/Sender.java
+++ src/main/java/com/example/smsfacade/sender/Sender.java
@@ -3,9 +3,15 @@ package com.example.smsfacade.sender;
 import com.example.smsfacade.bean.Message;
 
 /**
+ * 发送短信
  * @author 孙继峰
  * @since 2020/3/31
  */
 public interface Sender {
+    /**
+     * 发送短信
+     * @param message 短信内容
+     * @return 发送是否成功
+     */
     boolean send(Message message);
 }
